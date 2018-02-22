ca3622506179b1668700760454938fe6a45f2318
Razeral <ng.shangru@gmail.com>
2018-02-22 14:36:55 +0800

    first commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.FormBot.sln -p:DeployOnBuild=true -p:PublishProfile=wsdbot-Web-Deploy.pubxml -p:Password=nBefbFJc2G1TvBQk2qy9KgaR1HKh6QelbaDxMafLRuGqkSMQm9x8TSrbWbhQ
+
