be5fcd735e2b4419f83548301dcf5da7ad47e217
Razeral <ng.shangru@gmail.com>
2018-02-23 00:01:48 +0800

    1

--- Dialogs/EchoDialog.cs
+++ Dialogs/EchoDialog.cs
@@ -41,8 +41,15 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
                 CloudBlobContainer container = blobClient.GetContainerReference(System.Environment.GetEnvironmentVariable("AzureBlobStorageContainerReference"));
                 var blobRef = message.Conversation.Id + message.Timestamp.ToString();
                 CloudBlockBlob blockBlob = container.GetBlockBlobReference(blobRef);
-                blockBlob.Properties.ContentType = message.Attachments[0].ContentType;
-                blockBlob.SetProperties();
+                try
+                {
+                    blockBlob.Properties.ContentType = message.Attachments[0].ContentType;
+                    blockBlob.SetProperties();
+                }
+                catch (Exception e)
+                {
+                    System.Diagnostics.Trace.TraceError(e.Message);
+                }
                 System.Diagnostics.Trace.TraceInformation("In attachment path - after blob setup: BlobRef = " + blobRef);
                 System.Diagnostics.Trace.TraceInformation("ContentURL - " + message.Attachments[0].ContentUrl);
 
