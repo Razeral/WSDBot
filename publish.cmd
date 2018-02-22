1962f5c9eb7e6a385791c3ada50fdfcbe4741426
Razeral <ng.shangru@gmail.com>
2018-02-22 15:12:19 +0800

    new

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=wsdbot-ae5b-Web-Deploy.pubxml -p:Password=AKTxbQmdrri185kiprm0vvjXdPLertbqvo5XlymMteB8a1BuLA5pC4Poe3af
+
